3eaa091cb74caddc7744292f47e1fc7762909805
Iurii <iurii12345@gmail.com>
2022-08-12 12:25:13 +0400

    settings

--- app/roulette/settings.py
+++ app/roulette/settings.py
@@ -75,12 +75,6 @@ WSGI_APPLICATION = 'roulette.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     "default": {
         "ENGINE": os.environ.get("DB_ENGINE", "django.db.backends.sqlite3"),
